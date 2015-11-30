7a19fa6f8ec7c2ff4b0e3fc74c2ad5c90f6f5c93
Zmahmood <zafarullahmahmood@gmail.com>
2015-12-01 01:24:27 +0530

    Added bouncing arrow

--- static_in_pro/our_static/js/script.js
+++ static_in_pro/our_static/js/script.js
@@ -1,57 +1,68 @@
 jQuery(function( $ ){
-	/**
-	 * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
-	 * @see http://demos.flesler.com/jquery/scrollTo/
-	 * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.localScroll.
-	 */
-	
-	// The default axis is 'y', but in this demo, I want to scroll both
-	// You can modify any default like this
-	//$.localScroll.defaults.axis = 'y';
-	
-	/**
-	 * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
-	 * also affect the >> and << links. I want every link in the page to scroll.
-	 */
+
 	$.localScroll({
-		//target: '.canvas', // could be a selector or a jQuery object too.
-		//queue:true,
 		duration:500
-		//hash:true,
 	});
 });
 
 
 
-  var body = document.body,
-    html = document.documentElement;
+ $(document).ready(function() {
+	function setHeight() {
+		windowHeight = $(window).innerHeight();
+		$('.main-jumbotron').css('min-height', windowHeight);
+		};
+		setHeight();
 
-var height = Math.max( body.scrollHeight, 
-                        html.scrollHeight  );
+		$(window).resize(function() {
+		setHeight();
+		});
 
+		function setArrowHeight() {
+		jumboHeight = $('.main-jumbotron').innerHeight();
+		$('.arrow').css('top', jumboHeight-50);
+		};
+		setArrowHeight();
 
-  $(document).ready(function() {
-  function setHeight() {
-    windowHeight = $(window).innerHeight();
-    $('.main-jumbotron').css('min-height', windowHeight);
-  };
-  setHeight();
-  
-  $(window).resize(function() {
-    setHeight();
-  });
-
-    function setArrowHeight() {
-    jumboHeight = $('.main-jumbotron').innerHeight();
-    $('.arrow').css('top', jumboHeight-50);
-  };
-  setArrowHeight();
-  
-  $(window).resize(function() {
-    setArrowHeight();
-  });
-
-
+		$(window).resize(function() {
+		setArrowHeight();
+		});
 
 });
 
+
+$(document).ready(function() {
+	
+	var defaults = {
+		containerID: 'toTop', // fading element id
+		containerHoverID: 'toTopHover', // fading element hover id
+		scrollSpeed: 12000,
+		easingType: 'linear' 
+		};
+	
+	
+	$().UItoTop({ easingType: 'easeOutQuart' });
+	
+});       
+$(document).ready(function() {
+    var navoffeset = $(".navigation").offset().top;
+    $(window).scroll(function() {
+        var scrollpos = $(window).scrollTop();
+        //var scrollpos= $(window).innerHeight();
+        if (scrollpos >= navoffeset) {
+            $(".navigation").addClass("fixed");
+
+        } else {
+            $(".navigation").removeClass("fixed");
+        }
+    });
+
+});
+
+
+
+
+
+               
+
+            
